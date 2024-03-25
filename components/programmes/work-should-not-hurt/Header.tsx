bd6212b1d4a5a509cb499604dad821cf388c8a57
Jaromme <jarommelawn@gmail.com>
2024-03-25 20:48:40 +1300

    push

--- components/programmes/work-should-not-hurt/Header.tsx
+++ components/programmes/work-should-not-hurt/Header.tsx
@@ -8,11 +8,11 @@ const Header = () => {
             <div className="content-wrapper relative h-full">
               <div className="header-container flex flex-col h-full items-center justify-center gap-10">
                 <div className="programme-logo">
-                    <Image src="/programmes/logos/wsnh.svg" alt="work should not hurt logo" width='200' height='300'></Image>
+                    <Image src="/programmes/logos/wsnh.svg" alt="work should not hurt logo" width='200' height='300' className='w-full'></Image>
                 </div>
                 <div className="header-text-block flex flex-col gap-6 text-center w-full lg:w-3/4">
-                  <h2 className='font-bold text-4xl lg:text-8xl '>Work Should Not Hurt</h2>
-                  <p className='text-xl font-light'>The Work Should Not Hurt programme aims to make sure tradespeople can enjoy long, pain free careers and retire when they want to and not when forced to due to pain and injury.</p>
+                  <h2 className='font-bold text-3xl lg:text-8xl '>Work Should Not Hurt</h2>
+                  <p className='text-base md:text-xl font-light'>The Work Should Not Hurt programme aims to make sure tradespeople can enjoy long, pain free careers and retire when they want to and not when forced to due to pain and injury.</p>
                 </div>
               </div>
             </div>

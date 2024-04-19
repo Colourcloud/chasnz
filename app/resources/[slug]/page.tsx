7c21bde37715b7c74057d4a6f47aa1648bfa6dbe
Colourcloud <jarommelawn@gmail.com>
2024-04-19 13:28:24 +1200

    puah

--- app/resources/[slug]/page.tsx
+++ app/resources/[slug]/page.tsx
@@ -6,10 +6,32 @@ import { IoArrowBackOutline } from "react-icons/io5";
 import { Metadata } from 'next';
 import Head from 'next/head';
 
-export const generateMetadata = ({ params }: { params: { slug: string } }): Metadata => ({
-  title: `Chasnz - ${params.slug.replace(/-/g, ' ').split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')}`,
-  description: 'Resources',
-});
+// export const generateMetadata = ({ params }: { params: { slug: string } }): Metadata => ({
+//   title: `Chasnz - ${params.slug.replace(/-/g, ' ').split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')}`,
+//   description: `${params.slug.replace(/-/g, ' ')}`,
+// });
+
+
+type Props = {
+  params: { slug: string };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const url = `https://cms.chasnz.org/wp-json/wp/v2/posts?_embed&slug=${params.slug}`;
+  const response = await fetch(url);
+  const resMetadata = await response.json();
+
+  // Assuming resMetadata is an array and we are interested in the first element
+  if (resMetadata.length > 0 && resMetadata[0]) {
+    return {
+      title: resMetadata[0].title.rendered,
+      description: resMetadata[0].excerpt.rendered, // Use excerpt for description
+    };
+  } else {
+    throw new Error("No data received from the API");
+  }
+}
+
 
 interface Post {
   slug: string;
@@ -52,27 +74,27 @@ const BlogPost: React.FC<{ params: { slug: string } }> = async ({ params }) => {
         <div className='content-wrapper'>
           <div className="article-wrapper max-w-[980px] mx-auto flex flex-col gap-6">
             <a href="javascript:window.history.back();" className='flex flex-row items-center gap-1 text-xl'><IoArrowBackOutline /> Back to Resources</a>
-                <div className='w-full'>
-                  {featuredMediaUrl && (
-                  <Image src={featuredMediaUrl} alt="Featured Media" className='w-full' height={800} width={600} />
-                )}
-                </div>
-                <span className="text-base text-[#9C9C9C]">
-                  {new Date(post.date).toLocaleDateString('en-US', {
-                    weekday: 'long',
-                    year: 'numeric',
-                    month: 'long',
-                    day: 'numeric',
-                  })}
-                </span>
-                <h1 className='text-3xl lg:text-4xl font-semibold'>{post.title.rendered}</h1>
-                <div dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}></div>
-                <div className='flex flex-col gap-5 post-content' dangerouslySetInnerHTML={{ __html: post.content.rendered }}></div>
-                {post.acf.download && (
-                  <a href={post.acf.download} className='text-white text-sm font-medium py-3 px-6 rounded-full bg-[--primary-colour] self-start'>{post.acf.button_label}</a>
-                )}
-              </div>
+            <div className='w-full'>
+              {featuredMediaUrl && (
+                <Image src={featuredMediaUrl} alt="Featured Media" className='w-full' height={800} width={600} />
+              )}
+            </div>
+            <span className="text-base text-[#9C9C9C]">
+              {new Date(post.date).toLocaleDateString('en-US', {
+                weekday: 'long',
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+              })}
+            </span>
+            <h1 className='text-3xl lg:text-4xl font-semibold'>{post.title.rendered}</h1>
+            <div dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}></div>
+            <div className='flex flex-col gap-5 post-content' dangerouslySetInnerHTML={{ __html: post.content.rendered }}></div>
+            {post.acf.download && (
+              <a href={post.acf.download} className='text-white text-sm font-medium py-3 px-6 rounded-full bg-[--primary-colour] self-start'>{post.acf.button_label}</a>
+            )}
           </div>
+        </div>
       </div>
     </>
   );

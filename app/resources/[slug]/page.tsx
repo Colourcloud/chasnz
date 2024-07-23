c416afbf8af4aaf27cf95bacdee56361002e6191
Colourcloud <jarommelawn@gmail.com>
2024-07-24 00:33:44 +1200

    post

--- app/resources/[slug]/page.tsx
+++ app/resources/[slug]/page.tsx
@@ -92,9 +92,9 @@ const BlogPost: React.FC<{ params: { slug: string } }> = async ({ params }) => {
             </span>
             <h1 className='text-3xl lg:text-4xl font-semibold'>{post.title.rendered}</h1>
             <div className='flex flex-col gap-6 post-content' dangerouslySetInnerHTML={{ __html: post.content.rendered }}></div>
-
+            {/* check to see if the post download field exist */}
+            {post.acf.download && (
             <div className={`hs-web-interactive-inline text-white text-sm font-medium py-3 px-6 rounded-full bg-[--primary-colour] self-start`} data-hubspot-wrapper-cta-id={post.acf.ctaid}>
-              {post.acf.download && (
                 <a 
                   href={post.acf.download} 
                   className={`hs-inline-web-interactive-${post.acf.ctaid}`} 
@@ -104,10 +104,8 @@ const BlogPost: React.FC<{ params: { slug: string } }> = async ({ params }) => {
                 >
                   {post.acf.button_label}
                 </a>
-              )}
             </div>
-
-            
+            )}
             {/* <div className="hs-web-interactive-inline" data-hubspot-wrapper-cta-id="169009420371">
               {post.acf.download && (
                 <a href={post.acf.download} className='text-white text-sm font-medium py-3 px-6 rounded-full bg-[--primary-colour] self-start' target="_blank" rel="noopener" className="hs-inline-web-interactive-169009420371   " data-hubspot-cta-id="169009420371">{post.acf.button_label}</a>

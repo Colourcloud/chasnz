2828f7cbfab3940ab14c4b0648096426483d721f
Colourcloud <jarommelawn@gmail.com>
2024-08-07 12:54:45 +1200

    push

--- components/energy-wheel/Flipbook.tsx
+++ components/energy-wheel/Flipbook.tsx
@@ -7,15 +7,22 @@ const Flipbook = () => {
 
         <div className="flipbook-container bg-gray-500 w-full h-[700px] flex items-center justify-center">
           <iframe
+            src="https://cms.chasnz.org/energy-wheel/"
+            width="100%"
+            height="100%"
+            className="pdf-frame"
+          >
+          </iframe>
+        </div>
+        {/* <div className="flipbook-container bg-gray-500 w-full h-[700px] flex items-center justify-center">
+          <iframe
             src="https://cms.chasnz.org/wp-content/uploads/2024/07/18877-CHASNZ-Guide-to-High-Energy-Hazards-Guide-Jan-24-Update-F2-No-Bleed-1.pdf"
             width="100%"
             height="100%"
             className="pdf-frame"
           >
-            
-            
           </iframe>
-        </div>
+        </div> */}
       </div>
     </section>
   );

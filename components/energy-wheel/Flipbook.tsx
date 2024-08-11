88f54122f286f68712eafe89833a9ceacb02c569
Colourcloud <jarommelawn@gmail.com>
2024-08-12 11:27:51 +1200

    push

--- components/energy-wheel/Flipbook.tsx
+++ components/energy-wheel/Flipbook.tsx
@@ -5,7 +5,7 @@ const Flipbook = () => {
     <section className="flipbook py-20 lg:py-32">
       <div className="content-wrapper">
 
-        <div className="flipbook-container bg-gray-500 w-full h-[700px] flex items-center justify-center">
+        {/* <div className="flipbook-container bg-gray-500 w-full h-[700px] flex items-center justify-center">
           <iframe
             src="https://cms.chasnz.org/energy-wheel/"
             width="100%"
@@ -13,9 +13,9 @@ const Flipbook = () => {
             className="pdf-frame"
           >
           </iframe>
-        </div>
-        {/* <script src="https://cdn.commoninja.com/sdk/latest/commonninja.js" defer></script>
-        <div className="commonninja_component pid-2a96bbf2-ddf4-48bc-a2e5-62f075e58c04"></div> */}
+        </div> */}
+        <script src="https://cdn.commoninja.com/sdk/latest/commonninja.js" defer></script>
+        <div className="commonninja_component pid-2a96bbf2-ddf4-48bc-a2e5-62f075e58c04"></div>
       </div>
     </section>
   );

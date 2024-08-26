cc60b3a743330a25317de8ffc526b1da0273a70c
Colourcloud <jarommelawn@gmail.com>
2024-08-26 12:52:39 +1200

    push

--- components/energy-wheel/Header.tsx
+++ components/energy-wheel/Header.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
-import Image from 'next/image'
 
 const Header = () => {
   return (
     <div>
-        <div className="programmes-header relative header-gradient" id="energy-wheel-header">
-            <div className="content-wrapper relative h-full">
-              <div className="header-container flex flex-col h-full items-center justify-center lg:justify-end gap-10">
-                <div className="programme-logo w-3/4 lg:w-full flex justify-center lg:justify-start lg:pb-8 z-10">
-                    
-                </div>
-              </div>
+      <div className="programmes-header relative header-gradient" id="energy-wheel-header">
+        <video
+          className="absolute top-0 left-0 w-full h-full object-cover"
+          src="/energy-wheel/header-video.mp4"
+          autoPlay
+          loop
+          muted
+          playsInline
+        />
+        <div className="content-wrapper relative h-full">
+          <div className="header-container flex flex-col h-full items-center justify-center lg:justify-end gap-10">
+            <div className="programme-logo w-3/4 lg:w-full flex justify-center lg:justify-start lg:pb-8 z-10">
+              {/* Your content here */}
             </div>
+          </div>
         </div>
+      </div>
     </div>
   )
 }

99f08ddd44e9369c5fc5f79de8d89f43dc5d045c
Colourcloud <jarommelawn@gmail.com>
2024-10-03 11:05:10 +1300

    push

--- components/energy-wheel/Header.tsx
+++ components/energy-wheel/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div>
       <div className="programmes-header relative header-gradient" id="energy-wheel-header">
-        <video
+        {/* <video
           className="absolute top-0 left-0 w-full h-full object-cover hidden invisible md:visible md:block"
           src="/energy-wheel/header-video.webm"
           autoPlay
@@ -19,12 +19,10 @@ const Header = () => {
           loop
           muted
           playsInline
-        />
+        /> */}
         <div className="content-wrapper relative h-full">
-          <div className="header-container flex flex-col h-full items-center justify-center lg:justify-end gap-10">
-            <div className="programme-logo w-3/4 lg:w-full flex justify-center lg:justify-start lg:pb-8 z-10">
-              {/* Your content here */}
-            </div>
+          <div className="header-container flex flex-col h-full items-center justify-center gap-10">
+            <h1 className='text-2xl md:text-4xl lg:text-7xl font-bold'>Energy Based <span className="text-[--primary-colour]">Safety</span></h1>
           </div>
         </div>
       </div>

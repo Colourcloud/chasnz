6e9f4a63c3f2a5247926f9b0399815740593ee37
Colourcloud <jarommelawn@gmail.com>
2024-10-11 14:26:41 +1300

    push

--- components/events/energy-based-safety/Header.tsx
+++ components/events/energy-based-safety/Header.tsx
@@ -6,8 +6,16 @@ const Header = () => {
     <div>
         <div className="programmes-header relative header-gradient" id="energy-based-safety-header">
           <div className="content-wrapper relative h-full">
-              <div className="header-container flex flex-col h-full items-center justify-center gap-6 relative z-10 text-center max-w-6xl mx-auto">
-                    <h1 className='text-3xl md:text-4xl lg:text-5xl font-semibold !leading-tight'>Take part in our <span className='text-[--primary-colour]'>FREE</span> Energy Based Safety Workshops in November. Limited Spaces, Register Now!</h1>
+              <div className="header-container flex flex-col h-full items-center justify-center gap-6 relative z-10 text-center">
+                    <h1 className='text-3xl md:text-4xl lg:text-5xl font-semibold !leading-tight max-w-6xl mx-auto'>Take part in our <span className='text-[--primary-colour]'>FREE</span> Energy Based Safety Workshops in November. Limited Spaces, Register Now!</h1>
+                    <div className='flex flex-col gap-4 mt-6'>
+                      <h6 className='text-xl md:text-3xl font-semibold'>What is Energy Based Safety?</h6>
+                      <ul className="flex flex-col gap-4 items-center justify-center">
+                        <li className='text-lg md:text-xl font-semibold'>Energy Wheel</li>
+                        <li className='text-lg md:text-xl font-semibold'>Low Energy v High Energy Hazards</li>
+                        <li className='text-lg md:text-xl font-semibold'>High Energy Control Assessments</li>
+                      </ul>
+                   </div>
               </div>
             </div>
         </div>
--- components/events/energy-based-safety/Introduction.tsx
+++ components/events/energy-based-safety/Introduction.tsx
@@ -17,7 +17,7 @@ const Introduction = () => {
         <div className="site-wrapper">
             <div className="flex flex-col md:flex-row justify-between gap-16 items-center">
                 <div className="flex flex-col gap-6 w-full md:w-2/4">
-                    <h3 className='text-3xl md:text-4xl lg:text-5xl font-semibold !leading-tight'>Learn all about Energy Based Safety to help improve hazard recognition</h3>
+                    <h3 className='text-3xl md:text-4xl lg:text-5xl font-semibold !leading-tight'>Learn about Energy Based Safety to help improve safety performance</h3>
                     <p className='text-base md:text-lg'>There are three different categories of energy and potential &apos;high&apos; energy hazards, direct controls and alternative controls associated with each hazard. The 10 categories are within the green energy wheel.</p>
                     <p className='text-lg font-semibold md:text-xl text-[--text-colour]'>A few topics we wil be covering:</p>
                     <ul className='list-disc list-inside flex flex-col gap-2 text-[--text-colour]'>

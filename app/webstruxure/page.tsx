5e0683aedf4212099b7c3347b1d6029967720f9b
Colourcloud <jarommelawn@gmail.com>
2024-11-20 11:14:54 +1300

    webstruxure testing page

---
+++ app/webstruxure/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Metadata } from 'next'
+import Navigation from '@/components/common/Navigation'
+import Footer from '@/components/common/Footer'
+import Subscribe from '@/components/common/Subscribe'
+
+export const metadata: Metadata = {
+    title: 'Webstruxure Testing Page',
+}
+
+const page = () => {
+  return (
+    <>
+    <Navigation />
+    <section className="h-[100px] bg-black"></section>
+    <section className="h-screen bg-white"></section>
+    <Subscribe />
+    <Footer />
+    </>
+  )
+}
+
+export default page

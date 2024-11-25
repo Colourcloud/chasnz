de8def08715040aaeb9241b090b0920f3b9e5ef7
Colourcloud <jarommelawn@gmail.com>
2024-11-26 12:30:01 +1300

    fix console error

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,37 +60,38 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <Script strategy="beforeInteractive">
-        {`
-        (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:5094245,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        `}
-      </Script>
-
-      <Script>
-        {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-KJKG2QM');
-        `}
-      </Script>
-
-      </head>
-      <body className={poppins.className}>
-        <Script>
+        <Script strategy="beforeInteractive">
           {`
-          <iframe src=https://www.googletagmanager.com/ns.html?id=GTM-KJKG2QM
-            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+          (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:5094245,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
           `}
         </Script>
+
+        <Script strategy="afterInteractive" id="gtm-script">
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KJKG2QM');
+          `}
+        </Script>
+      </head>
+      <body className={poppins.className}>
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KJKG2QM"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <GoogleAnalytics />
         <NextTopLoader color="#7ABC25" showSpinner={false}/>
         <Newsletter />
@@ -99,14 +100,20 @@ export default function RootLayout({
         {children}
         <Footer />
         <Script
-            type="text/javascript"
-            id="hs-script-loader"
-            async
-            defer
-            src="//js.hsforms.net/forms/v2.js"
-          ></Script>
-          <Script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/40083784.js"></Script>
-        </body>
+          type="text/javascript"
+          id="hs-forms-script"
+          async
+          defer
+          src="//js.hsforms.net/forms/v2.js"
+        />
+        <Script 
+          type="text/javascript" 
+          id="hs-script-loader" 
+          async 
+          defer 
+          src="//js.hs-scripts.com/40083784.js"
+        />
+      </body>
     </html>
   );
 }

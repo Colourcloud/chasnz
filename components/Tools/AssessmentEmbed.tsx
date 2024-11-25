23d37aa75eb6656831639db3d5dccc231f13c09b
Colourcloud <jarommelawn@gmail.com>
2024-11-25 19:03:05 +1300

    possible fix?

--- components/Tools/AssessmentEmbed.tsx
+++ components/Tools/AssessmentEmbed.tsx
@@ -25,7 +25,7 @@ export default function AssessmentEmbed({ userEmail }: AssessmentEmbedProps) {
       const appOrigin = window.location.origin;
       const childOrigin = "https://dev.chasnz.dotnous.co.nz";
       const email = getEmailFromCookie();
-      const iFrameSrc = `${childOrigin}/assessment.aspx?parentOrigin=${appOrigin}&email=${email}`;
+      const iFrameSrc = childOrigin + "/assessment.aspx" + "?parentOrigin=" + appOrigin+'&email=trevor@webstruxure.co.nz';
       
       if (iframeRef.current) {
         iframeRef.current.src = iFrameSrc;

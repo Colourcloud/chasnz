90ab5e2e5cd0634bfba339e5350ca1b2680830f6
Colourcloud <jarommelawn@gmail.com>
2025-03-17 23:26:40 +1300

    push

--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -10,11 +10,25 @@ const Header = () => {
         <div className="home-header">
             <div className="introduction-video">
               <div className="site-wrapper h-full">
-              <div className="header-text relative z-10 h-full flex flex-col justify-center pb-14 items-start">
-                    <div className='flex flex-col gap-8 items-left w-full lg:w-1/3 text-left px-4 lg:px-0'>
-                        <h1 className='text-4xl lg:text-5xl text-white font-semibold text-shadow-lg'><span className='text-white md:text-[#F6A21A] bg-[#F6A21A] md:bg-transparent px-1 md:px-0'>Have a hmmm</span> to stay injury-free in the workplace.</h1>
-                        <p className='text-lg md:text-xl font-base'><span className='text-white md:text-[#F6A21A] bg-[#F6A21A] md:bg-transparent px-1 md:px-0'>We've partnered with ACC</span> on a suite of videos showcasing tips to stay injury-free so you can safely get the job done.</p>
-                        <Link href="/have-a-hmmm"><p className='text-lg md:text-xl font-semibold flex flex-row gap-2 items-center'><FaCirclePlay className='text-[#F6A21A] text-3xl' /> Click here for more details</p></Link>
+              <div className="header-text relative z-10 h-full flex flex-col lg:flex-row justify-center items-center gap-6 mt-4 md:mt-20 lg:mt-0">
+                    <div className='flex flex-col gap-8 items-left w-full lg:w-2/4 text-left px-4 lg:px-0'>
+                      <span className='text-black bg-[#F6A21A] text-sm uppercase font-bold py-2 px-3 self-start'>Free Tool</span>
+                        <h1 className='text-4xl lg:text-6xl text-white font-bold text-shadow-lg'><span className='text-[#F6A21A]'>WSNH Foundation</span> Assessment Toolkit</h1>
+                        <p className='text-lg md:text-xl font-base'>Assess your organisation&apos;s ability to recognise and <span className='text-[#F6A21A]'>address musculoskeletal injuries in your workplace</span> and you&apos;ll get your very own detailed report</p>
+                        <Link href="/work-should-not-hurt/foundation-assessment-toolkit-musculoskeletal-injuries"><p className='text-lg md:text-xl font-semibold flex flex-row gap-2 items-center'><FaCirclePlay className='text-[#F6A21A] text-3xl' /> Click here for more details</p></Link>
+                    </div>
+                    <div className="w-full lg:w-2/4 rounded-lg border-2 border-[#F6A21A] overflow-hidden relative">
+                      <div className="absolute top-0 right-0 bg-[#F6A21A] text-black uppercase text-xs md:text-sm rounded-bl-lg px-3 md:px-4 py-1 md:py-2 font-bold z-10"><span>New Tool</span></div>
+                      <video
+                        autoPlay
+                        muted
+                        loop
+                        playsInline
+                        className='w-full relative'
+                      >
+                        <source src="/home/wsnh-assessment-graphic.mp4" type="video/mp4" />
+                        Your browser does not support the video tag.
+                      </video>
                     </div>
                 </div>
               </div>
